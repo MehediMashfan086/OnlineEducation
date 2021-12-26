cd7e511dc5e6023e83723b23aa2ce2b42469f0e4
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-12-26 22:13:48 +0600

    Saving to GitHub

--- edu/urls.py
+++ edu/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('notes/', views.notes, name='notes'),
+    path('homework/', views.homework, name='homework'),
+    path('youtube/', views.youtube, name='youtube'),
+    path('todo/', views.todo, name='todo'),
+    path('books/', views.books, name='books'),
+    path('dictionary/', views.dictionary, name='dictionary'),
+    path('wiki/', views.wiki, name='wiki'),
+    path('conversion/', views.conversion, name='conversion'),
 ]
--- edu/views.py
+++ edu/views.py
@@ -3,3 +3,27 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'edu/home.html')
+
+def notes(request):
+    return render(request, 'edu/notes.html')
+
+def homework(request):
+    return render(request, 'edu/homework.html')
+
+def youtube(request):
+    return render(request, 'edu/youtube.html')
+
+def todo(request):
+    return render(request, 'edu/todo.html')
+
+def books(request):
+    return render(request, 'edu/books.html')
+
+def dictionary(request):
+    return render(request, 'edu/dictionary.html')
+
+def wiki(request):
+    return render(request, 'edu/wiki.html')
+
+def conversion(request):
+    return render(request, 'edu/conversion.html')
