ec0c881b139571eba3cce9763ef6f20e4c7ae0cc
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-12-28 00:25:23 +0600

    Saving to GitHub

--- edu/views.py
+++ edu/views.py
@@ -4,6 +4,7 @@ from .models import *
 from .forms import *
 from django.contrib import messages
 from django.views import generic
+from youtubesearchpython import VideosSearch
 
 # Create your views here.
 def home(request):
@@ -75,7 +76,32 @@ def delete_homework(request, pk = None):
     return redirect('homework')
 
 def youtube(request):
-    form = DashboardForm()
+    if request.method == "POST":
+        form = DashboardForm(request.POST)
+        text = request.POST['text']
+        video = VideosSearch(text, limit = 20)
+        result_list = []
+        for i in video.result()['result']:
+            result_dict = {
+                'input': text,
+                'title': i['title'],
+                'duration': i['duration'],
+                'thumbnail': i['thumbnails'][0]['url'],
+                'channel': i['channel']['name'],
+                'link': i['link'],
+                'views': i['viewCount']['short'],
+                'published': i['publishedTime'],
+            }
+            desc = ''
+            if i['descriptionSnippet']:
+                for j in i['descriptionSnippet']:
+                    desc += j['text']
+            result_dict['description'] = desc
+            result_list.append(result_dict)
+            context = {'form': form, 'results': result_list}
+        return render(request, 'edu/youtube.html', context)
+    else:
+        form = DashboardForm()
     context = {'form': form}
     return render(request, 'edu/youtube.html', context)
 
