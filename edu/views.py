577c9d27c4430768433153a7e9fb4daae3211ec2
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-12-29 01:32:20 +0600

    Saving to GitHub

--- edu/views.py
+++ edu/views.py
@@ -290,6 +290,14 @@ def conversion(request):
     return render(request, 'edu/conversion.html', context)
 
 def register(request):
-    form = UserRegistrationForm()
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Created for {username} Successfully !!')
+            #redirect("login")
+    else:
+        form = UserRegistrationForm()
     context = {'form': form}
     return render(request, 'edu/register.html', context)
