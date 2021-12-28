69bf09329b933f05a98a08741ada2be0205e1222
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-12-29 02:53:49 +0600

    Saving to GitHub

--- OnlineEducationStudy/urls.py
+++ OnlineEducationStudy/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('register/', edu_views.register, name = 'register'),
     path('login/',auth_views.LoginView.as_view(template_name = 
     "edu/login.html"), name = 'login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name = 
+    "edu/logout.html"), name = 'logout'),
     path('profile/', edu_views.profile, name = 'profile'),
 ]
--- edu/views.py
+++ edu/views.py
@@ -303,4 +303,22 @@ def register(request):
     return render(request, 'edu/register.html', context)
 
 def profile(request):
-    return render(request, 'edu/profile.html')
+    homeworks = Homework.objects.filter(is_finished = False, user = request.user)
+    todos = Todo.objects.filter(is_finished = False, user = request.user)
+    if len(homeworks) == 0:
+        homework_done = True
+    else:
+        homework_done = False
+        
+    if len(todos) == 0:
+        todos_done = True
+    else:
+        todos_done = False
+    
+    context = {
+        'homeworks': homeworks,
+        'todos': todos,
+        'homework_done': homework_done,
+        'todos_done': todos_done
+    }
+    return render(request, 'edu/profile.html', context)
