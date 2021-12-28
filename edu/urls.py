a9ddb4d9595eeacdcbb93f3b41cbed447bf05271
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-12-28 15:44:44 +0600

    Saving to GitHub

--- edu/urls.py
+++ edu/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
     path('delete_todo/<int:pk>', views.delete_todo, name='delete-todo'),
     
     path('books/', views.books, name='books'),
+    
     path('dictionary/', views.dictionary, name='dictionary'),
     path('wiki/', views.wiki, name='wiki'),
     path('conversion/', views.conversion, name='conversion'),
--- edu/views.py
+++ edu/views.py
@@ -5,6 +5,7 @@ from .forms import *
 from django.contrib import messages
 from django.views import generic
 from youtubesearchpython import VideosSearch
+import requests
 
 # Create your views here.
 def home(request):
@@ -147,7 +148,32 @@ def delete_todo(request, pk = None):
     return redirect('todo')
 
 def books(request):
-    return render(request, 'edu/books.html')
+    if request.method == "POST":
+        form = DashboardForm(request.POST)
+        text = request.POST['text']
+        url = "https://www.googleapis.com/books/v1/volumes?q="+text
+        r = requests.get(url)
+        answer = r.json()
+        result_list = []
+        for i in range(10):
+            result_dict = {
+                'title': answer['items'][i]['volumeInfo']['title'],
+                'subtitle': answer['items'][i]['volumeInfo'].get('subtitle'),
+                'description': answer['items'][i]['volumeInfo'].get('description'),
+                'count': answer['items'][i]['volumeInfo'].get('pageCount'),
+                'categories': answer['items'][i]['volumeInfo'].get('categories'),
+                'rating': answer['items'][i]['volumeInfo'].get('pageRating'),
+                'thumbnail': answer['items'][i]['volumeInfo'].get('imageLinks').get('thumbnail'),
+                'preview': answer['items'][i]['volumeInfo'].get('previewLink'),
+            }
+            
+            result_list.append(result_dict)
+            context = {'form': form, 'results': result_list}
+        return render(request, 'edu/books.html', context)
+    else:
+        form = DashboardForm()
+    context = {'form': form}
+    return render(request, 'edu/books.html', context)
 
 def dictionary(request):
     return render(request, 'edu/dictionary.html')
